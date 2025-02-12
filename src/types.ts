7388a7c7cfdcfac0c8f385f478f848bb9388d748
Arthurr-Rosante <arrosante@gmail.com>
2025-02-12 20:56:41 -0300

    Major Concept Overhaul

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,5 @@
 export interface CleanOpts {
   showLog?: boolean;
-}
-
-export interface ScheduleOpts {
-  name: string;
-  days: number[];
-  hour: number;
-  default?: boolean;
-}
-
-export interface ScheduledTask {
-  id: string;
-  name: string;
-  days: number[];
-  hour: number;
-  active: boolean;
+  path?: string | string[];
+  name?: string;
 }
