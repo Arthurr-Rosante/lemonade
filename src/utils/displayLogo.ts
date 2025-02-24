8d0446459e240627f8e7f5d716b360efcb5311c5
Arthurr-Rosante <arrosante@gmail.com>
2025-02-23 22:02:15 -0300

    Ensuring Logo will always appear

--- src/utils/displayLogo.ts
+++ src/utils/displayLogo.ts
@@ -1,18 +1,46 @@
-import chalk from "chalk";
-import fs from "fs-extra";
 import path from "path";
+import os from "os";
+import fse from "fs-extra";
+import { CONFIG_FOLDER } from "./config.js";
+import chalk from "chalk";
 import { fileURLToPath } from "url";
 
+const LOGO_FILE = path.join(CONFIG_FOLDER, "logo.txt");
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export function displayLogo(): void {
-  const logoPath = path.join(__dirname, "../assets/logo.txt");
+export function displayLogo() {
+  const logo = fse.readFileSync(
+    path.join(__dirname, "../assets/logo.txt"),
+    "utf-8"
+  );
 
-  try {
-    const logo = fs.readFileSync(logoPath, "utf-8");
-    console.log(chalk.greenBright(logo));
-  } catch (error) {
-    console.log(chalk.red("⚠️  Logo file not found!"));
+  if (!fse.existsSync(CONFIG_FOLDER)) {
+    fse.mkdirSync(CONFIG_FOLDER, { recursive: true });
   }
+
+  if (!fse.existsSync(LOGO_FILE)) {
+    fse.writeFileSync(LOGO_FILE, logo, "utf-8");
+  }
+
+  console.log(chalk.greenBright(logo));
 }
+
+// import chalk from "chalk";
+// import fs from "fs-extra";
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// export function displayLogo(): void {
+//   const logoPath = path.join(__dirname, "../assets/logo.txt");
+
+//   try {
+//     const logo = fs.readFileSync(logoPath, "utf-8");
+//     console.log(chalk.greenBright(logo));
+//   } catch (error) {
+//     console.log(chalk.red("⚠️  Logo file not found!"));
+//   }
+// }
